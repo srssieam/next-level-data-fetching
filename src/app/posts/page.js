7959d842514add286bd8687e80bc2c0969e03e75
srs-sieam <srssieam@gmail.com>
2023-12-02 19:03:58 +0600

    explore built-in CSS MOdule support

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react';
+import styles from "./Posts.module.css"
 
 const PostPage = async () => {
 
@@ -12,7 +12,7 @@ const PostPage = async () => {
 
     return (
         <div className='w-full'>
-            <h1>Total Post: {posts.length}</h1>
+            <h1 className={styles.header_text}>Total Post: {posts.length}</h1>
             {
                 posts.map(post => <div key={post.id} className="text-black card w-[50%] mx-auto my-5 bg-yellow-100 shadow-xl">
                     <div className="card-body">
---
+++ src/app/users/page.js
@@ -0,0 +1,13 @@
+
+import Users from '@/components/Users/Users';
+import React from 'react';
+
+const UsersPage = () => {
+    return (
+        <div>
+            <Users></Users>
+        </div>
+    );
+};
+
+export default UsersPage;
